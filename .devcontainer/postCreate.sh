48edfef1d7af8fe428a19a9b0c21297d9655b1b0
Daniel Kraemer <dkraemer@dkross.org>
2020-07-01 16:55:51 +0000

    Better defaults and README

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,9 +5,19 @@ set -o xtrace
 
 cp -fv .devcontainer/.bashrc /root/
 
-yarn global add typescript
-yarn global add @types/node
-yarn global add ts-node
-yarn global add tsdx
-yarn global add oclif
-yarn global add gulp
+# TypeScript development
+#yarn global add typescript
+#yarn global add @types/node
+#yarn global add tsdx
+#yarn global add ts-node
+#yarn global add oclif
+
+# Misc
+#yarn global add gulp
+
+# Angular
+#yarn global add @angular/cli
+#yarn global add http-server
+
+# Google Firebase
+#yarn global add firebase-tools
